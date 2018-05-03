f6d2dbed94e41dd8aff87e2124eeec78a03ab158
Grace Shih <vocalise@gmail.com>
2018-05-03 02:56:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\s\w{5}\s/).strip
+  fiveLetterWords = text.scan(/\s\w{5}\s/)
+  for fiveletterWords
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
