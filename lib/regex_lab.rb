c6048ef29a57c1d4dc5efd3c0457d31efc55ac1c
Grace Shih <vocalise@gmail.com>
2018-05-03 03:40:56 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -32,5 +32,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-   
+   phone.each do |number|
+     if match(/\d{3}.*\d{3}.*\d{4}/) == nil
+       return false
+     else
+       return true
+     end
 end
