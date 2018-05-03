d2e1a92f7eea9d2100b685df91cf483bcfa49907
Grace Shih <vocalise@gmail.com>
2018-05-03 03:06:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ end
 def words_five_letters_long(text)
   words = text.split()
   fiveLetterWords = words.grep(/\w{5}\s/)
-  return words
+  return fiveLetterWords
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
