e05701d36b0536befa866e84684dff79ffbf3356
Grace Shih <vocalise@gmail.com>
2018-05-03 03:42:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -32,10 +32,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-   puts phone
    if phone.match(/\d{3}.*\d{3}.*\d{4}/) == nil
-       return false
-     else
-       return true
-     end
+      return false
+   else
+      return true
+   end
 end
