d584c4869ac2dbd36dc70bfa6ef16684f63ece3c
Grace Shih <vocalise@gmail.com>
2018-05-03 03:22:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,7 +22,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-   if text.match(/^[A-Z]+.*[:punct:]*$/) == false
+   if text.match(/^[A-Z]+.*[:punct:]*$/) == nil
      return false
    else
      return true
