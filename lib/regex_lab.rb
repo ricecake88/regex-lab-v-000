fd05358da86c74bffa7467119efd6e39cc6d7fbf
Grace Shih <vocalise@gmail.com>
2018-05-03 03:18:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,7 +22,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-   return text.match(/^[A-Z]+.*[:punct:]/)
+   return text.match(/^[A-Z]+.*[:punct:]$/)
 end
 
 def valid_phone_number?(phone)
