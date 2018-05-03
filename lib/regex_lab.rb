25234f84832f50871d277be4576cab5a75c69099
Grace Shih <vocalise@gmail.com>
2018-05-03 03:05:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,8 +11,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  words = []
-  fiveLetterWords = text.scan(/\s\w{5}\s/)
+  words = text.split()
+  fiveLetterWords = text.grep(/\w{5}\s/)
   fiveLetterWords.each { |x| words.push(x.strip) }
   return words
 end
